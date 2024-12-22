6fcda7326f45604933ac585379cdc73431ebb952
Mahathi <97830392+ragupathi09@users.noreply.github.com>
2024-12-22 21:11:18 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/api/hello/call', methods=['GET'])
 def hello():
     return jsonify(message="Hello, World!")
+@app.route('/api', methods=['GET'])
+def hello():
+    return jsonify(message="Hello, Student!")
 
 if __name__ == '__main__':
     app.run(debug=True)
